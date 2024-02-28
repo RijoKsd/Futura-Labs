f0b5655a0d41e095ef1a97e36277cf11a0800686
Rijo Sebastian <rijobdk@gmail.com>
2024-02-28 19:00:16 +0530

    Complete digit sum program

---
+++ C/digitSum.c
@@ -0,0 +1,14 @@
+// Program to print digit sum;
+#include <stdio.h>
+int main(){
+    int num, remainder, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    while(num != 0){
+        remainder = num % 10;
+        sum += remainder;
+        num /= 10;
+    }
+    printf("%d", sum);
+    return 0;
+}
