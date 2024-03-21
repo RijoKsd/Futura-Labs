b457cdb1220fd62295a36d19268071a4c68a35ad
Rijo Sebastian <rijobdk@gmail.com>
2024-03-21 20:56:23 +0530

    Add form validation program

---
+++ Javascript/script.js
@@ -0,0 +1,22 @@
+const submitBtnEl = document.getElementById("submitBtn");
+const emailEl = document.getElementById("email");
+const passwordEl = document.getElementById("password");
+let error = document.querySelector(".error");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (emailEl.value === "" || passwordEl.value === "") {
+    error.textContent = "Please fill out all fields";
+    error.style.color = "red";
+  } else if (!emailRegex.test(emailEl.value)) {
+    error.textContent = "Please enter a valid email";
+  } else if (passwordEl.value.length < 6) {
+    error.textContent = "Password must be at least 6 characters";
+  } else if (passwordEl.value === "password") {
+    error.textContent = "Password cannot be password";
+  }else {
+    error.textContent = "";
+  }
+});
