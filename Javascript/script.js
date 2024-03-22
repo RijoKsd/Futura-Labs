d40ce787d9fbc8911d6a97b7b6ac6e44a26ac809
Rijo Sebastian <rijobdk@gmail.com>
2024-03-22 14:44:07 +0530

    Add Right Half Pyramid

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,13 +1,14 @@
 const submitBtnEl = document.getElementById("submitBtn");
 const emailEl = document.getElementById("email");
 const passwordEl = document.getElementById("password");
+const confirmPwd = document.getElementById("confirm-password");
 let error = document.querySelector(".error");
 let form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  if (emailEl.value === "" || passwordEl.value === "") {
+  if (emailEl.value === "" || passwordEl.value === "" || confirmPwd === "") {
     error.textContent = "Please fill out all fields";
     error.style.color = "red";
   } else if (!emailRegex.test(emailEl.value)) {
@@ -16,7 +17,11 @@ form.addEventListener("submit", (e) => {
     error.textContent = "Password must be at least 6 characters";
   } else if (passwordEl.value === "password") {
     error.textContent = "Password cannot be password";
-  }else {
+  } else {
     error.textContent = "";
+  }
+
+  if (passwordEl.value !== confirmPwd.value) {
+    error.textContent = "Password does not match";
   }
 });
